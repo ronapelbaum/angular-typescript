20382dc43c52c8f450b7c9876c7e85f5097a260e
Ron Apelbaum <ron.apelbaum@hpe.com>
2016-03-28 14:08:15 +0300

    add angular $timeout

--- src/index.ts
+++ src/index.ts
@@ -4,26 +4,29 @@
 //import 'lib.ts';
 
 
-function App () {
-  return {
-    restrict: 'E',
-    template: '<h1>{{ App.message }}</h1>',
-    controller: AppController,
-    controllerAs: 'App'
-  }
+function App() {
+    return {
+        restrict: 'E',
+        template: '<h1>{{ App.message }}</h1>',
+        controller: AppController,
+        controllerAs: 'App'
+    }
 }
 
 class AppController {
 
-  message: string;
+    message:string;
 
-  constructor() {
-    this.message = "Angular + TypeScript + Webpcak Minimal Starter!";
-  }
+    static $inject = ['$timeout'];
+
+    constructor(private $timeout) {
+        this.message = "Angular + TypeScript + Webpcak Minimal Starter!";
+        $timeout(()=>this.message = "goodbye", 2000);
+    }
 
 }
 
 angular.module('app', ['utils'])
-.directive('app', App);
+    .directive('app', App);
 
 //angular.bootstrap(document, ['app']);
