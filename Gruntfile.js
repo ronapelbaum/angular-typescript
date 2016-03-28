a43d8b788ab27a0803bff2edbbeee7458fc60e5b
Ron Apelbaum <ron.apelbaum@hpe.com>
2016-03-28 13:41:48 +0300

    add dev mode and default mode (concat)

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,13 +2,18 @@ module.exports = function (grunt) {
     const src = {
         main: "src/**/*",
         ts: "src/**/*.ts",
-        index: "src/index.html"
+        indexFile: "src/index.html",
+        lib:[
+            "node_modules/angular/angular.js"
+        ]
     };
     const dist = {
         main: "dist/",
-        lib: "dist/lib/",
-        ts: "dist/ts",
-        index: "dist/index.html"
+        libDir: "dist/lib/",
+        libFile: "dist/lib.js",
+        tsDir: "dist/ts/",
+        indexFile: "dist/index.html",
+        productFile: "dist/product.js"
     };
 
     grunt.initConfig({
@@ -18,21 +23,31 @@ module.exports = function (grunt) {
         'ts': {
             default: {
                 src: [src.ts],
-                outDir: dist.ts
+                outDir: dist.tsDir
             }
         },
         'copy': {
             index: {
-                src: [src.index],
-                dest: dist.index
+                src: [src.indexFile],
+                dest: dist.indexFile
             },
             lib: {
-                src: "node_modules/angular/angular.js",
-                dest: dist.lib
+                src: src.lib,
+                dest: dist.libDir
+            }
+        },
+        concat: {
+            product: {
+                src: dist.tsDir + "/**/*.js",
+                dest: dist.productFile
+            },
+            lib: {
+                src: src.lib,
+                dest: dist.libFile
             }
         },
         'sails-linker': {
-            defaultOptions: {
+            dev: {
                 options: {
                     startTag: '<!--SCRIPTS-->',
                     endTag: '<!--SCRIPTS END-->',
@@ -41,7 +56,19 @@ module.exports = function (grunt) {
                 },
                 files: {
                     // Target-specific file lists and/or options go here.
-                    'dist/index.html': [dist.lib + "/**/*.js", dist.ts + "/**/*.js"]
+                    'dist/index.html': [dist.libDir + "/**/*.js", dist.tsDir + "/**/*.js"]
+                }
+            },
+            default: {
+                options: {
+                    startTag: '<!--SCRIPTS-->',
+                    endTag: '<!--SCRIPTS END-->',
+                    fileTmpl: '<script type="text/javascript" src="%s"></script>',
+                    appRoot: dist.main
+                },
+                files: {
+                    // Target-specific file lists and/or options go here.
+                    'dist/index.html': [dist.libFile, dist.productFile]
                 }
             }
         },
@@ -59,6 +86,8 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-sails-linker');
     grunt.loadNpmTasks('grunt-sails-linker');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
 
-    grunt.registerTask("default", ["clean", "ts", "copy", "sails-linker"]);
+    grunt.registerTask("dev", ["clean", "ts", "copy", "sails-linker:dev"]);
+    grunt.registerTask("default", ["clean", "ts", "copy","concat", "sails-linker:default"]);
 };
